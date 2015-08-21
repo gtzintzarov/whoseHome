61d47dcdc3450b905b7bad6a2a50adebad1cffb1
George Tzintzarov <gtzintzarov@gmail.com>
2015-08-21 01:19:33 -0400

    From playground

---
+++ currentNetwork.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+
+class networkUser:
+	def __init__(self)
+
+
---
+++ importMACS.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+import csv
+with open('MACS', 'rb') as csvfile:
+	MACreader = csv.reader(csvfile, delimiter='	')
+	
+	for row in MACreader:
+		print row[1]
