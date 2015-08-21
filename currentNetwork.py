2476c5b84b3cc39080649cb4a84475340546dadf
unknown <gtzintzarov@gmail.com>
2015-08-21 02:06:51 -0400

    working towards syncing two files

--- currentNetwork.py
+++ currentNetwork.py
@@ -1,6 +1,13 @@
 #! /usr/bin/python
 
+
+
 class networkUser:
-	def __init__(self)
+	def __init__(self,MAC_addr):
+		self.myMAC = MAC_addr
+
+
+
+
 
 
--- importMACS.py
+++ importMACS.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python
 
 import csv
+import currentNetwork
 
-
+myMACS = []
 with open('MACS', 'rb') as csvfile:
 	MACreader = csv.reader(csvfile, delimiter=' ')
 	for row in MACreader:
+		myMACS.append(networkUser(row[3]))
 		print row[3]
 
