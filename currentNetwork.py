1e8ee1d12f08bcefd82aa57f96125919b4e1f906
unknown <gtzintzarov@gmail.com>
2015-08-21 02:47:57 -0400

    first hack at it

--- currentNetwork.py
+++ currentNetwork.py
@@ -1,7 +1,6 @@
 #! /usr/bin/python
 
 
-
 class networkUser:
 	def __init__(self,MAC_addr):
 		self.myMAC = MAC_addr
--- importMACS.py
+++ importMACS.py
@@ -1,12 +1,22 @@
 #! /usr/bin/python
 
 import csv
-import currentNetwork
+import RPi.GPIO as GPIO
 
-myMACS = []
+class networkUser:
+	def __init__(self, MAC_addr):
+		self.myMAC = MAC_addr
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(13, GPIO.OUT)
+
+
+MACSonNET = []
+Edwin = 'a4:5e:60:cb:4e:43'
+
 with open('MACS', 'rb') as csvfile:
 	MACreader = csv.reader(csvfile, delimiter=' ')
 	for row in MACreader:
-		myMACS.append(networkUser(row[3]))
-		print row[3]
-
+		MACSonNET.append(networkUser(row[3]))
+		if Edwin == row[3]:
+			GPIO.output(13, GPIO.HIGH)
